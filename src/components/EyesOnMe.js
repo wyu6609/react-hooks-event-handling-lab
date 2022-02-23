a9b66bd2750b0db49e82e173d704ce66a5e1343c
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-02-23 13:37:20 -0500

    assignment completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+const EyesOnMe = () => {
+  return (
+    <button
+      onFocus={() => {
+        console.log("Good!");
+      }}
+      onBlur={() => {
+        console.log("Hey! Eyes on me!");
+      }}
+    >
+      Eyes on me
+    </button>
+  );
+};
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+import React from "react";
+
+const Keypad = () => {
+  function keyPadHandler() {
+    console.log("Entering password...");
+  }
+  return <input onChange={keyPadHandler} type="password" />;
+};
+
+export default Keypad;
